9f097a7ef9895e98c40dd1282d0add5af4c76e55
Sriramgosikonda <gosikondasriram7@gmail.com>
2024-05-26 19:50:51 +0530

    Your commit message

---
+++ script.js
@@ -0,0 +1,13 @@
+function togglemenu() {
+    const menu = document.querySelector('.menu-links');
+    const icon = document.querySelector(".hamburger-icon");
+    
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+    
+    if (menu.classList.contains("open")) {
+        menu.style.maxHeight = `${menu.scrollHeight}px`;
+    } else {
+        menu.style.maxHeight = "0px";
+    }
+}
